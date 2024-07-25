5db531bff69ddcbd2e25d9bb74c9717d46e32d73
intern <mounikajalem76@gmai.com>
2024-07-25 13:58:00 +0530

    image Upload on firebase

---
+++ app/src/main/java/com/example/imageuploadonfirebase/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.imageuploadonfirebase;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button uploadbtn,showbtn;
+    ProgressBar progressBar;
+    DatabaseReference root= FirebaseDatabase.getInstance().getReference("Image");
+    StorageReference reference= FirebaseStorage.getInstance().getReference();
+    Uri imageuri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        imageView=(ImageView) findViewById(R.id.imageview);
+        uploadbtn=(Button) findViewById(R.id.upload);
+        showbtn=(Button) findViewById(R.id.show);
+        progressBar=(ProgressBar) findViewById(R.id.progressbar);
+
+        progressBar.setVisibility(View.INVISIBLE);
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent=new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,2);
+            }
+        });
+        uploadbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (imageuri !=null){
+                    uploadToFirebase(imageuri);
+
+                }else {
+                    Toast.makeText(MainActivity.this, "Please select Image", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==2 && resultCode==RESULT_OK && data !=null){
+            imageuri= data.getData();
+            imageView.setImageURI(imageuri);
+
+        }
+    }
+    private void uploadToFirebase(Uri uri){
+        StorageReference fileRef=reference.child(System.currentTimeMillis() +"." + getFileExtension(uri));
+        fileRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        model model1=new model(uri.toString());
+                        String modelId= root.push().getKey();
+                        root.child(modelId).setValue(model1);
+                        Toast.makeText(MainActivity.this, "Image Uploading Successfully", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                progressBar.setVisibility(View.VISIBLE);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(MainActivity.this, "Uploading failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    private String getFileExtension(Uri mUri){
+        ContentResolver cr=getContentResolver();
+        MimeTypeMap mime=MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(mUri));
+    }
+
+}
---
+++ app/src/main/java/com/example/imageuploadonfirebase/model.java
@@ -0,0 +1,19 @@
+package com.example.imageuploadonfirebase;
+
+public class model {
+    String imageUrl;
+    public model(){
+
+    }
+    public model(String imageUrl){
+        this.imageUrl=imageUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
